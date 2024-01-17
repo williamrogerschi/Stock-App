77fe585ff1af775faa0712c3f8a2734e72deca9d
Billy Rogers <william.rogers.chi@gmail.com>
2024-01-17 10:27:16 -0600

    getting base components and fonts setup

---
+++ stock/src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './home.css'
+
+const Home = () => {
+  return (
+    <div className='home'>
+        <h2>This is Home</h2>
+    </div>
+  )
+}
+
+export default Home
