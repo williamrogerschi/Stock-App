ae5f07cc5999877d87dcca03454aa89ad70feb47
Billy Rogers <william.rogers.chi@gmail.com>
2024-01-17 21:30:45 -0600

    cant access chart but everything else is done

--- stock/src/api/stock-api.js
+++ stock/src/api/stock-api.js
@@ -1,5 +1,5 @@
-const basePath = 'https://finnhub.io/api/v1'
-const apiKey = 'cmk4j21r01qi6gqum5dgcmk4j21r01qi6gqum5e0'
+export const basePath = 'https://finnhub.io/api/v1'
+export const apiKey = 'cmk9m6hr01qi6gquq3h0cmk9m6hr01qi6gquq3hg'
 
 
 export const searchSymbol = async (query) => {
@@ -43,6 +43,9 @@ export const searchSymbol = async (query) => {
   
 
   export const fetchHistoricalData = async ( stockSymbol, resolution, from, to ) => {
+    const encodedSymbol = encodeURIComponent(stockSymbol)
+    console.log('encdoed:', encodedSymbol)
+
     const url = `${basePath}/stock/candle?symbol=${stockSymbol}&resolution=${resolution}&from=${from}&to=${to}&token=${apiKey}`;
     const response = await fetch(url)
     console.log('fetchHistoricalData:', response)
