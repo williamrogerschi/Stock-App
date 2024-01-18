f70b86ee93dbe42a5caeffcc2b7377ce0f619c9f
Billy Rogers <william.rogers.chi@gmail.com>
2024-01-17 18:08:29 -0600

    small styling changes

--- stock/src/components/ChartFilter.jsx
+++ stock/src/components/ChartFilter.jsx
@@ -5,7 +5,7 @@ const ChartFilter = ({ text, active, onClick }) => {
 
   return (
 
-    <button onClick={onClick} className={`w-12 m-2 h-8 border-1 rounded-md flex items-center justify-center cursor-pointer ${active ? 'bg-indigo-600 border-indigo-700 text-gray-100' : 'border-indigo-300 text-indigo-300 hover:bg-indigo-400'}`}
+    <button onClick={onClick} className={`w-12 m-2 h-8 border-1 rounded-md flex items-center justify-center cursor-pointer ${active ? 'bg-indigo-600 border-indigo-700 text-gray-100' : 'border-gray-800 text-gray-600 hover:bg-indigo-600 hover:text-gray-100'}`}
     >
         {text}
     </button>
--- stock/src/components/Overview.jsx
+++ stock/src/components/Overview.jsx
@@ -6,18 +6,18 @@ const Overview = ({ symbol, price, change, changePercent, currency }) => {
   return (
 
     <Card>
-        <span className='absolute left-4 top-4 text-neutral-400 text-lg xl:text-xl 2xl:text-2xl'>
+        <span className='absolute left-4 top-4 text-neutral-600 text-md xl:text-lg 2xl:text-xl'>
             {symbol}
         </span>
         <div className='w-full h-full flex items-center justify-around'>
-            <span className='text-2xl xl:text-4xl 2xl:text-5xl flex items-center'>
+            <span className='text-2xl xl:text-4xl 2xl:text-5xl flex items-center pt-6'>
                 ${price}
                 <span className='text-lg xl:text-xl 2xl:text-2xl text-neutral-400 m-2'>
                     {currency}
                 </span>
             </span>
             <span 
-                className={`text-lg xl:text-xl 2xl:text-2xl ${change > 0 ? "text-lime-500" : "text-red-500"}`}>{change} <span>({changePercent}%)</span>
+                className={`text-lg xl:text-xl 2xl:text-2xl pt-6 ${change > 0 ? "text-lime-500" : "text-red-500"}`}>{change} <span>({changePercent}%)</span>
             </span>
         </div>
     </Card>
